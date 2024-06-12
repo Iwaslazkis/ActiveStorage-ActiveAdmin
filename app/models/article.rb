6e3044076b5089640602fc076529ef85a8763e5c
Your Name <you@example.com>
2024-06-12 17:54:30 +0000

    Setup Active Admin, and Sassc, and Devise

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["body", "created_at", "id", "id_value", "title", "updated_at"]
+    end
+
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 end
